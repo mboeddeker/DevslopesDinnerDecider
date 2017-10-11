9d4c42f6c9a7e5da72ba41e4bcf24adce4aa1007
Marvin Böddeker <marvin.boeddeker@gsmg.de>
2017-10-11 22:53:07 +0200

    Decide Btn Funktionen

--- app/src/main/java/de/appgewaltig/devslopesdinnerdecider/MainActivity.kt
+++ app/src/main/java/de/appgewaltig/devslopesdinnerdecider/MainActivity.kt
@@ -2,13 +2,24 @@ package de.appgewaltig.devslopesdinnerdecider
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    // MAIN ACTIVITY
+    val foodList = arrayListOf("Chinese","Hamburger","Pizza","Mc Donald's", "Tacco Bell")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener {
+            val random = Random()
+            var randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+        }
+
     }
+
+
 }
