0ae22b9459a586dfbbeed0802ba01e07c157b13a
Marvin Böddeker <marvin.boeddeker@gsmg.de>
2017-10-11 23:30:52 +0200

    Finished

--- app/src/main/java/de/appgewaltig/devslopesdinnerdecider/MainActivity.kt
+++ app/src/main/java/de/appgewaltig/devslopesdinnerdecider/MainActivity.kt
@@ -2,6 +2,7 @@ package de.appgewaltig.devslopesdinnerdecider
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -19,7 +20,21 @@ class MainActivity : AppCompatActivity() {
             selectedFoodTxt.text = foodList[randomFood]
         }
 
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
+            if (!newFood.isEmpty()){
+                foodList.add(newFood)
+                println(foodList)
+                addFoodTxt.text.clear()
+            } else {
+                Toast.makeText(this, "Bitte auch etwas eingeben", Toast.LENGTH_SHORT).show()
+                addFoodTxt.text.clear()
+            }
+        }
+
     }
+
+
 
 
 }
