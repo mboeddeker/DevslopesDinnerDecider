3a560b1c3dc9f270ee6ef4df68d1df62168bfa88
Marvin Böddeker <marvin.boeddeker@gsmg.de>
2017-10-11 18:22:32 +0200

    Init

--- app/src/main/java/de/appgewaltig/devslopesdinnerdecider/MainActivity.kt
+++ app/src/main/java/de/appgewaltig/devslopesdinnerdecider/MainActivity.kt
@@ -1,9 +1,11 @@
 package de.appgewaltig.devslopesdinnerdecider
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    // MAIN ACTIVITY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
